9a8a32785d824743a885fc653df07cfb474eda6e
Robert B. Gramacy <bobby@statslab.cam.ac.uk>
2008-09-18 00:00:00 +0000

    version 0.1-4

--- src/convhulln.c
+++ src/convhulln.c
@@ -25,6 +25,10 @@
 * Changed the interface for R
 */
 
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <R.h>
 #include <Rdefines.h>
 #include "qhull_a.h"
@@ -44,135 +48,140 @@ documentation for the available options.)\n\n\
 SEXP convhulln(const SEXP p, const SEXP options)
 {
   SEXP retval, area, vol, retlist, retnames;
-	int curlong, totlong, i, j, retlen;
-	unsigned int dim, n;
-	int exitcode; 
-	boolT ismalloc;
-	char flags[250];             /* option flags for qhull, see qh_opt.htm */
-	char *opts;
-	int *idx;
-	double *pt_array;
+  int curlong, totlong, i, j, retlen;
+  unsigned int dim, n;
+  int exitcode; 
+  boolT ismalloc;
+  char flags[250];             /* option flags for qhull, see qh_opt.htm */
+  char *opts;
+  int *idx;
+  double *pt_array;
+  struct stat file_status;
 
-	/* Bobby */
-	area = vol = retlist = NULL;
-	retlen = 1;
 
-	/* Bobby */ /*FILE *outfile = stdout; */     /* output from qh_produce_output() use NULL to skip qh_produce_output() */
-	FILE *outfile = fopen("qhull_out.txt", "a"); /* Bobby */
-	FILE *errfile = stderr;      /* error messages from qhull code */
+  /* Bobby */
+  area = vol = retlist = NULL;
+  retlen = 1;
 
-	retval = R_NilValue;
+  /* Bobby */ /*FILE *outfile = stdout; */     /* output from qh_produce_output() use NULL to skip qh_produce_output() */
+  FILE *outfile = fopen("qhull_out.txt", "a"); /* Bobby */
+  FILE *errfile = stderr;      /* error messages from qhull code */
 
-	if(!isString(options) || length(options) != 1){
-		error("Second argument must be a single string.");
-	}
-	if(!isMatrix(p) || !isReal(p)){
-		error("First argument should be a real matrix.");
-	}
+  retval = R_NilValue;
 
-	i=LENGTH(STRING_ELT(options,0));
-	opts = (char *) R_alloc( ((i>1)?i:1), sizeof(char) );
-	strcpy(opts, " ");
-	if(i>1) strcpy(opts, CHAR(STRING_ELT(options,0)));
+  if(!isString(options) || length(options) != 1){
+    error("Second argument must be a single string.");
+  }
+  if(!isMatrix(p) || !isReal(p)){
+    error("First argument should be a real matrix.");
+  }
 
-	dim = ncols(p);
-	n   = nrows(p);
-	if(dim <= 0 || n <= 0){
-		error("Invalid input matrix.");
-	}
+  i=LENGTH(STRING_ELT(options,0));
+  opts = (char *) R_alloc( ((i>1)?i:1), sizeof(char) );
+  strcpy(opts, " ");
+  if(i>1) strcpy(opts, CHAR(STRING_ELT(options,0)));
 
-	j=0;
-	pt_array = (double *) R_alloc(n*dim, sizeof(double)); 
-	for(i=0; i < n; i++)
-		for(j=0; j < dim; j++)
-			pt_array[dim*i+j] = REAL(p)[i+n*j]; /* could have been pt_array = REAL(p) if p had been transposed */
+  dim = ncols(p);
+  n   = nrows(p);
+  if(dim <= 0 || n <= 0){
+    error("Invalid input matrix.");
+  }
 
-	ismalloc = False;   /* True if qhull should free points in qh_freeqhull() or reallocation */
+  j=0;
+  pt_array = (double *) R_alloc(n*dim, sizeof(double)); 
+  for(i=0; i < n; i++)
+    for(j=0; j < dim; j++)
+      pt_array[dim*i+j] = REAL(p)[i+n*j]; /* could have been pt_array = REAL(p) if p had been transposed */
 
-	/* hmm  lot's of options for qhull here */
-	// sprintf(flags,"qhull Qt Tcv %s",opts); // removed by Bobby
-	sprintf(flags,"qhull Qt %s",opts);  // Bobby moved Tcv to default options
-	/* outfile = NULL; this produces nonsensical output ... 
-	   needs to be fixed to get convexhulln quiet */
-	exitcode = qh_new_qhull (dim,n,pt_array,ismalloc,flags,outfile,errfile);
-	/* If you want some debugging information replace the NULL
-	// pointer with stdout
-	*/
+  ismalloc = False;   /* True if qhull should free points in qh_freeqhull() or reallocation */
 
-	if (!exitcode) {  /* 0 if no error from qhull */
+  /* hmm  lot's of options for qhull here */
+  // sprintf(flags,"qhull Qt Tcv %s",opts); // removed by Bobby
+  sprintf(flags,"qhull Qt %s",opts);  // Bobby moved Tcv to default options
+  /* outfile = NULL; this produces nonsensical output ... 
+     needs to be fixed to get convexhulln quiet */
+  exitcode = qh_new_qhull (dim,n,pt_array,ismalloc,flags,outfile,errfile);
+  /* If you want some debugging information replace the NULL
+  // pointer with stdout
+  */
 
-		facetT *facet;                  /* set by FORALLfacets */
-		vertexT *vertex, **vertexp;		/* set by FORALLfacets */
-		unsigned int n = qh num_facets;
+  if (!exitcode) {  /* 0 if no error from qhull */
 
-		PROTECT(retval = allocMatrix(INTSXP, n, dim));
-		idx = (int *) R_alloc(n*dim,sizeof(int));
+    facetT *facet;                  /* set by FORALLfacets */
+    vertexT *vertex, **vertexp;		/* set by FORALLfacets */
+    unsigned int n = qh num_facets;
 
-		qh_vertexneighbors();
+    PROTECT(retval = allocMatrix(INTSXP, n, dim));
+    idx = (int *) R_alloc(n*dim,sizeof(int));
 
-		i=0;
-		FORALLfacets {
-			j=0;
-			/*std::cout << "Current index " << i << "," << j << std::endl << std::flush;
-			// qh_printfacet(stdout,facet);
-			*/
-			FOREACHvertex_ (facet->vertices) {
-				/* qh_printvertex(stdout,vertex); */
-				if (j >= dim)
-					warning("extra vertex %d of facet %d = %d",
-					j++,i,1+qh_pointid(vertex->point));
-				else
-					idx[i+n*j++] = 1 + qh_pointid(vertex->point);
-			}
-			if (j < dim) warning("facet %d only has %d vertices",i,j);
-			i++;
-		}
-		j=0;
-		for(i=0;i<nrows(retval);i++)
-			for(j=0;j<ncols(retval);j++)
-				INTEGER(retval)[i+nrows(retval)*j] = idx[i+n*j];
+    qh_vertexneighbors();
 
-		/* Bobby: return area and volume */
-		if (qh totarea != 0.0) {
-		  PROTECT(area = allocVector(REALSXP, 1));
-		  REAL(area)[0] = qh totarea;
-		  retlen++;
-		}
-		if (qh totvol != 0.0) {
-		  PROTECT(vol = allocVector(REALSXP, 1));
-		  REAL(vol)[0] = qh totvol;
-		  retlen++;
-		}
+    i=0;
+    FORALLfacets {
+      j=0;
+      /*std::cout << "Current index " << i << "," << j << std::endl << std::flush;
+      // qh_printfacet(stdout,facet);
+      */
+      FOREACHvertex_ (facet->vertices) {
+	/* qh_printvertex(stdout,vertex); */
+	if (j >= dim)
+	  warning("extra vertex %d of facet %d = %d",
+		  j++,i,1+qh_pointid(vertex->point));
+	else
+	  idx[i+n*j++] = 1 + qh_pointid(vertex->point);
+      }
+      if (j < dim) warning("facet %d only has %d vertices",i,j);
+      i++;
+    }
+    j=0;
+    for(i=0;i<nrows(retval);i++)
+      for(j=0;j<ncols(retval);j++)
+	INTEGER(retval)[i+nrows(retval)*j] = idx[i+n*j];
 
-		/* Bobby: make a list if there is area or volume */
-		if(retlen > 1) {
-		  PROTECT(retlist = allocVector(VECSXP, retlen));
-		  PROTECT(retnames = allocVector(VECSXP, retlen));
-		  retlen += 2;
-		  SET_VECTOR_ELT(retlist, 0, retval);
-		  SET_VECTOR_ELT(retnames, 0, mkChar("hull"));
-		  SET_VECTOR_ELT(retlist, 1, area);
-		  SET_VECTOR_ELT(retnames, 1, mkChar("area"));
-		  SET_VECTOR_ELT(retlist, 2, vol);
-		  SET_VECTOR_ELT(retnames, 2, mkChar("vol"));
-		  setAttrib(retlist, R_NamesSymbol, retnames);
-		} else retlist = retval;
+    /* Bobby: return area and volume */
+    if (qh totarea != 0.0) {
+      PROTECT(area = allocVector(REALSXP, 1));
+      REAL(area)[0] = qh totarea;
+      retlen++;
+    }
+    if (qh totvol != 0.0) {
+      PROTECT(vol = allocVector(REALSXP, 1));
+      REAL(vol)[0] = qh totvol;
+      retlen++;
+    }
 
-		/* Bobby */
-		UNPROTECT(retlen);
-		/* UNPROTECT(1); */
-	}
-	qh_freeqhull(!qh_ALL);					/*free long memory */
-	qh_memfreeshort (&curlong, &totlong);	/* free short memory and memory allocator */
+    /* Bobby: make a list if there is area or volume */
+    if(retlen > 1) {
+      PROTECT(retlist = allocVector(VECSXP, retlen));
+      PROTECT(retnames = allocVector(VECSXP, retlen));
+      retlen += 2;
+      SET_VECTOR_ELT(retlist, 0, retval);
+      SET_VECTOR_ELT(retnames, 0, mkChar("hull"));
+      SET_VECTOR_ELT(retlist, 1, area);
+      SET_VECTOR_ELT(retnames, 1, mkChar("area"));
+      SET_VECTOR_ELT(retlist, 2, vol);
+      SET_VECTOR_ELT(retnames, 2, mkChar("vol"));
+      setAttrib(retlist, R_NamesSymbol, retnames);
+    } else retlist = retval;
 
-	if (curlong || totlong) {
-		warning("convhulln: did not free %d bytes of long memory (%d pieces)",
-			totlong, curlong);
-	}
+    /* Bobby */
+    UNPROTECT(retlen);
+    /* UNPROTECT(1); */
+  }
+  qh_freeqhull(!qh_ALL);					/*free long memory */
+  qh_memfreeshort (&curlong, &totlong);	/* free short memory and memory allocator */
 
-	fclose(outfile); /* Bobby */
+  if (curlong || totlong) {
+    warning("convhulln: did not free %d bytes of long memory (%d pieces)",
+	    totlong, curlong);
+  }
 
-	/* Bobby: */
-	return retlist;
-	/* return retval; */
+  /* close the outfile, and possibly remove it if it is empty */
+  fclose(outfile); /* Bobby */
+  stat("qhull_out.txt", &file_status);
+  if((int) file_status.st_size == 0) unlink("qhull_out.txt");
+
+  /* Bobby: */
+  return retlist;
+  /* return retval; */
 }
